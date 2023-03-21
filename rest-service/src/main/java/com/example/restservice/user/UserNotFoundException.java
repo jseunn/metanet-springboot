9fd2aa8d546412febac3d78014be863cdfa0413b
seeun jeong <sesej4512@gmail.com>
2023-03-21 14:29:43 +0900

    feat: @ResponseStatus 사용하여 404 에러 적용

--- rest-service/src/main/java/com/example/restservice/user/UserNotFoundException.java
+++ rest-service/src/main/java/com/example/restservice/user/UserNotFoundException.java
@@ -1,8 +1,12 @@
 package com.example.restservice.user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 //2xx => OK
 //4xx => client error
 //5xx => server error
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException{
     // 예외클래스 생성
 
