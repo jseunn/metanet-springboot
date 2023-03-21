73e42cbf298f3f66736637747a2b2e8eeffe08d6
seeun jeong <sesej4512@gmail.com>
2023-03-21 16:26:04 +0900

    feat: 다국어 처리 추가

--- rest-service/src/main/java/com/example/restservice/helloworld/HelloController.java
+++ rest-service/src/main/java/com/example/restservice/helloworld/HelloController.java
@@ -1,24 +1,37 @@
 package com.example.restservice.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloController {
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World!!";
     }
 
     @GetMapping("/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
+    public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World Bean!!!");
     }
 
     @GetMapping("/hello-world-bean/path-variable/{name}")
-    public HelloWorldBean helloWorldBean2(@PathVariable String name){
+    public HelloWorldBean helloWorldBean2(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World Bean, %s", name));
+    }
+
+    @GetMapping("/hello-world-international")
+    public String helloWorldInternational(
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale){ // 언어에 대한 locale 정보를 클라이언트가 반드시 줘야 한다.
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 }
