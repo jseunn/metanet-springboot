44b9588058a74691cc126ec414d0a83f6315dc45
seeun jeong <sesej4512@gmail.com>
2023-03-22 11:13:36 +0900

    feat: UserRepository 생성, Controller에서 findAll test

---
+++ rest-service/src/main/java/com/example/restservice/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.restservice.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> { // JpaRepository만 상속받으면, CRUD 메서드 사용 가능
+
+}
