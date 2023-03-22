90b67ccb45401ad4e196c2c59f0f68c62dfc64f4
seeun jeong <sesej4512@gmail.com>
2023-03-22 11:34:22 +0900

    feat: user 하나 값 출력

--- rest-service/src/main/java/com/example/restservice/user/UserJpaController.java
+++ rest-service/src/main/java/com/example/restservice/user/UserJpaController.java
@@ -1,11 +1,14 @@
 package com.example.restservice.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/jpa")
@@ -17,4 +20,17 @@ public class UserJpaController {
     public List<User> retrieveAllUsers(){
         return userRepository.findAll();
     }
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id){
+        Optional<User> user = userRepository.findById(id);
+
+        if(!user.isPresent()){ // Optional 객체일 때는 null 이 아니라 isPresent
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return EntityModel.of(user.get(),
+                linkTo(methodOn(UserController.class).retrieveAllUsers()).withRel("all-users"));
+    }
+
 }
