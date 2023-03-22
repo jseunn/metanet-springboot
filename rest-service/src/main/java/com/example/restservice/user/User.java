9d84993a0594483006ff801386b566b6e774a4cd
seeun jeong <sesej4512@gmail.com>
2023-03-22 14:17:27 +0900

    feat: post 추가, 연관관계 어노테이션

--- rest-service/src/main/java/com/example/restservice/user/User.java
+++ rest-service/src/main/java/com/example/restservice/user/User.java
@@ -10,8 +10,10 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -31,4 +33,18 @@ public class User {
     //@JsonIgnore // 데이터값이 존재해도 노출시키지 않는다.
     private String password;
     private String ssn;
+
+    @OneToMany(mappedBy = "user") //Post에서의 User 변수명
+    private List<Post> posts; // 1:N 관계에서 데이터 가져오기
+
+    // 에러 -> List<Post> posts 변수가 추가되어 @AllArgsConstructor 사용했을 때
+    // 해당 생성자 없음 오류.. 생성자 따로 지정해주었음
+    
+    public User(Integer id, String name, Date joinDate, String password, String ssn) {
+        Id = id;
+        this.name = name;
+        this.joinDate = joinDate;
+        this.password = password;
+        this.ssn = ssn;
+    }
 }
